7e07e26a8370dd6379c9a6eac84fac1a6de45420
peiyang <sleepybear1125@163.com>
2020-08-09 00:27:02 +0800

    题库49.增加哈希解法

--- 49.字母异位词分组.ts
+++ 49.字母异位词分组.ts
@@ -28,5 +28,27 @@ function strSplitAndSort(str:string): string {
   arr.sort();//O(nlogn)
   return arr.join('');
 }
+
+
+// 解法2: 遍历+哈希,时间复杂度O(NK) 空间复杂度O(NK)，N为strs数组的长度，K为strs数组中字符串的最大长度
+function groupAnagrams1(strs: string[]): string[][] {
+  let dict = new Map<string, Array<string>>();
+  let startCode = 'a'.charCodeAt(0);
+  strs.forEach((v)=>{
+      let arr = Array(26).fill(0);
+      for(let j = 0; j < v.length; j++) {
+          arr[v.charCodeAt(j) - startCode]++;
+      } 
+      const hashKey = arr.join();
+      if (dict.has(hashKey)) {
+          const temp = dict.get(hashKey);
+          temp && temp.push(v);
+      } else {
+          dict.set(hashKey, [v]);
+      }
+  })
+  return [...dict.values()];
+}
+
 // @lc code=end
 
